5408ba1f9f1142ff682e62f24f971615c5518127
Giovanne-Villanueva <70661928+Giovanne-Villanueva@users.noreply.github.com>
2023-04-20 12:44:53 -0700

    Fixed error with reset button

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -71,7 +71,7 @@ $(function () {
   function clearText(event){
     var element = $(event.target);
     var textArea = (element.siblings(".description"));
-    textArea.empty();
+    textArea.replaceWith("<textarea class=\"col-7 col-md-9 description\" rows=\"3\"> </textarea>");
     //console.log(textArea.val())
     var attribute= element.parent().attr('id');
     localStorage.removeItem(attribute);
